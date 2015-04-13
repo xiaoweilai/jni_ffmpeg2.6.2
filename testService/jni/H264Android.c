729e95f6d3c5eea44fff3c9e3d17894956d52496
weixj <wxjlmr@126.com>
2015-04-13 22:10:18 +0800

    修改编译不通过的源文件H264Android.c,通过编译

--- testService/jni/H264Android.c
+++ testService/jni/H264Android.c
@@ -33,6 +33,74 @@ unsigned int *r_2_pix=NULL;
 unsigned int *g_2_pix=NULL;
 unsigned int *b_2_pix=NULL;
 
+
+
+
+
+
+static int decode_write_frame(AVCodecContext *avctx, AVFrame *frame, int *frame_count, AVPacket *pkt, int last)
+{
+	int h;
+	int w;
+    int len, got_frame;
+    char buf[1024];
+    len = avcodec_decode_video2(avctx, frame, &got_frame, pkt);
+    if (len < 0) {
+        printf("Error while decoding frame %d\n", *frame_count);
+        return len;
+    }
+    if (got_frame) {
+        printf("Saving frame %3d\n", *frame_count);
+
+//        QImage image(QSize(avctx->width, avctx->height), QImage::Format_RGB888);
+
+        for(h = 0; h < avctx->height; h++)
+        {
+            for(w = 0; w < avctx->width; w ++)
+            {
+                int hh = h >> 1;
+                int ww = w >> 1;
+                int Y = frame->data[0][h * frame->linesize[0] + w];
+                int U = frame->data[1][hh * (frame->linesize[1]) + ww];
+                int V = frame->data[2][hh * (frame->linesize[2]) + ww];
+
+                int C = Y - 16;
+                int D = U - 128;
+                int E = V - 128;
+
+                int r = 298 * C           + 409 * E + 128;
+                int g = 298 * C - 100 * D - 208 * E + 128;
+                int b = 298 * C + 516 * D           + 128;
+
+                r = qBound(0, r >> 8, 255);
+                g = qBound(0, g >> 8, 255);
+                b = qBound(0, b >> 8, 255);
+
+                r = qBound(0, r, 255);
+                g = qBound(0, g, 255);
+                b = qBound(0, b, 255);
+
+//                QRgb rgb = qRgb(r, g, b);
+//                image.setPixel(QPoint(w, h), rgb);
+            }
+        }
+
+//        QString fname = QString("img") + QString::number(*frame_count) + ".jpg";
+//        image.save(fname);
+
+
+        //pgm_save(frame->data[0], frame->linesize[0], avctx->width, avctx->height, buf);
+        (*frame_count)++;
+    }
+    if (pkt->data) {
+        pkt->size -= len;
+        pkt->data += len;
+    }
+    return 0;
+}
+
+
+
 void DeleteYUVTab()
 {
 //	av_free(tmp_pic);
@@ -182,16 +250,17 @@ JNIEXPORT jint JNICALL Java_com_zhutieju_testservice_H264Android_initDecoder
 
 	CreateYUVTab_16();
 	
-	avcodec_init();
+	//avcodec_init();
+
 	avcodec_register_all();
 	//找到H264解码器
 	codec = avcodec_find_decoder(CODEC_ID_H264);
 	//分配解码器内存
-	c = avcodec_alloc_context3();
+	c = avcodec_alloc_context3(codec);
 	//打开解码器
-	avcodec_open(c,codec);
+	avcodec_open2(c, codec, NULL);
 	//给解码器解码后的帧数据分配存储空间
-	picture  = avcodec_alloc_frame();//picture= malloc(sizeof(AVFrame));
+	picture  = av_frame_alloc();//picture= malloc(sizeof(AVFrame));
 
 	return 1;
 }
@@ -211,7 +280,7 @@ JNIEXPORT jint JNICALL Java_com_zhutieju_testservice_H264Android_dalDecoder
 	jbyte * Buf = (jbyte*)(*env)->GetByteArrayElements(env, in, 0);
 	jbyte * Pixel= (jbyte*)(*env)->GetByteArrayElements(env, out, 0);
 
-	int consumed_bytes = avcodec_decode_video(c, picture, &got_picture, Buf, nalLen);
+	int consumed_bytes = avcodec_decode_video2(c, picture, &got_picture, Buf);
 
 	if(got_picture > 0)
 	{
