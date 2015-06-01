3d3d9770050945bc3ac1dae75560450af73646be
yjduoduo <wxjlmr@126.com>
2015-06-01 21:05:24 +0800

    D:\android-ndk-r9d-windows-x86_64\android-ndk-r9d sample程序，有JNI的相关例子

---
+++ samples/hello-jni/obj/local/mips/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/mips/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ /cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/string.h \
+ /cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/cdefs.h \
+ /cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/android/api-level.h \
+ /cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/malloc.h \
+ /cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/jni.h
+
+/cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/string.h:
+
+/cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/cdefs.h:
+
+/cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/android/api-level.h:
+
+/cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/malloc.h:
+
+/cygdrive/d/android-ndk-r9d-windows-x86_64/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/jni.h:
